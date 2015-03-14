77b0d9fe0a7a21500279579d9b707c7863ca40ea
Marc Schifer <mschifer@mozilla.com>
2015-03-13 17:13:22 -0700

    collect bug numbers, only process files with matching extensions, add query support for post processing change data

--- mfs.sh
+++
@@ -1,7 +0,0 @@
-# Nightly
-cd ~/mozilla-central
-hg metrics -f ~/churn/nightly-38.json -d "2014-01-13 to 2015-02-10"
-#hg metrics -f ~/churn/nightly-37.json -d "2014-11-28 to 2015-01-12"
-#hg metrics -f ~/churn/nightly-36.json -d "2014-10-13 to 2014-11-27"
-#hg metrics -f ~/churn/nightly-35.json -d "2014-09-02 to 2014-10-12"
-
